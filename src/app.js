2b4fbd0104e2702f0dbe625a90b587d5975c6efa
Thomas <thomas.bogle@outlook.com>
2022-05-05 15:34:38 +0100

    Passed first test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
 const app = express();
+
 app.use(express.json());
 
+app.use('/artist', artistRouter);
+
 app.get('/', (req, res) => {
-    res.status(200).json({ result: `Hello World!` });
+    res.status(200).json({ result: 'Hello World!' });
 });
 
 module.exports = app;
