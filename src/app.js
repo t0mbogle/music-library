563db67b6242d817b704e83c886c9bedd1bff630
Thomas <thomas.bogle@outlook.com>
2022-05-10 16:04:18 +0100

    Create artist instance and SQL security

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/artist', artistRouter);
+// grab all artist.js file to handle endpoints that start with /artist
+// use artistRouter for everything that starts with /artist
+// POST a query to create instance of artist
 
 app.get('/', (req, res) => {
     res.status(200).json({ result: 'Hello World!' });
--- utils/create-database.js
+++ utils/create-database.js
@@ -33,6 +33,13 @@ const setUpDatabase = async () => {
 
     // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    await db.query(`USE ${DB_NAME}`);
+    await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+      id INT PRIMARY KEY auto_increment,
+      name VARCHAR(25),
+      genre VARCHAR(25)
+    )`);
+    // configures table, via SQL statement as a string, sent to database. 
     db.close();
 
   } catch (err) {
